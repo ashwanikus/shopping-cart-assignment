c648066a9ef1aaa426e09b8df181aa4c7a398479
Ashwani Kumar <ashwani.bietjhs@gmail.com>
2019-04-23 17:07:22 +0530

    cart-functionality added

--- src/server/express.js
+++ src/server/express.js
@@ -6,10 +6,14 @@ const webpack = require("webpack");
 const config = require("../../config/webpack.dev.js");
 const compiler = webpack(config);
 
-var bodyParser = require('body-parser');
-var products = require("../../json/products/index.get.json");
-var addtocart = require("../../json/addToCart/index.post.json");
-var categories = require("../../json/categories/index.get.json");
+let bodyParser = require('body-parser');
+
+let products = require("../../json/products/index.get.json");
+let addtocart = require("../../json/addToCart/index.post.json");
+let categories = require("../../json/categories/index.get.json");
+let banners = require("../../json/banners/index.get.json");
+let checkoutAmount = 0;
+let itemsInCart = [];
 
 // parse application/x-www-form-urlencoded
 server.use(bodyParser.urlencoded({ extended: false }));
@@ -38,37 +42,49 @@ server.get('/addtocart', function (req, res) {
 });
 
 server.get('/categories', function (req, res) {
-    
     categories.sort((a, b) => {
         return a.order - b.order;
     });
-    var new_cat = categories.filter((item) => {
+    let new_cat = categories.filter((item) => {
         return item.enabled;
     });
     res.end(JSON.stringify({ categories: new_cat }));
 });
 
 server.get('/banners', function (req, res) {
-    var banners = require("../../json/banners/index.get.json");
     res.end(JSON.stringify({ banners: banners }));
 });
 
 server.get('/products', function (req, res) {
-    var products = require("../../json/products/index.get.json");
     if (typeof req.query.id === "undefined") {
         res.end(JSON.stringify({ products: products }));
     } else {
-        var filteredProduct = products.filter((p) => p.category == req.query.id);
+        let filteredProduct = products.filter((p) => p.category == req.query.id);
         res.end(JSON.stringify({ products: filteredProduct }));
     }
 });
 
 server.get('/addTocart/:id', function (req, res) {
-    var products = require("../../json/products/index.get.json");
-    res.end(JSON.stringify({ id: req.params.id }));
+    products.forEach(element => {
+        if (element.id === req.params.id) {
+            if (element.count == undefined) {
+                element.count = 1;
+                element.totalPrice = element.price * element.count;
+                itemsInCart.push(element);
+            } else {
+                element.count = element.count + 1;
+                element.totalPrice = element.price * element.count;
+            }
+            res.end(JSON.stringify({ item_in_cart: itemsInCart }));
+        }
+    });
+});
+
+server.get('/cartitems', function (req, res) {
+    res.end(JSON.stringify({ item_in_cart: itemsInCart }));
 });
 
 server.listen(8080, () => {
-    console.log("server is listening");
+    console.log("Server is listening at port 8080");
 });
 
